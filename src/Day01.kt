8c910f6b24d0e712070c656de59c44c27c44c813
Harry Angus Cliff Jr <harryacliff@gmail.com>
2022-12-01 18:54:18 -0500

    Day 1 KT

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
 fun main() {
+    fun getTotalCaloriesList(input: List<String>): MutableList<Int> {
+        val totalCalorieList = mutableListOf<Int>()
+        val listDelimiter = ""
+
+        val breakLines = input.mapIndexedNotNull{ index, elem -> index.takeIf{ elem == listDelimiter } }
+        breakLines.forEachIndexed { index, breakLineInt ->
+            val startingIndex = if(index == 0){
+                0
+            } else {
+                breakLines[index-1]
+            }
+            val elfCalorieList = input.subList(startingIndex,breakLineInt).filterNot {
+                it == ""
+            }
+            totalCalorieList.add(elfCalorieList.sumOf {
+                it.toInt()
+            })
+        }
+        return totalCalorieList
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getTotalCaloriesList(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val totalCaloriesList = getTotalCaloriesList(input)
+        val topThreeTotalList = mutableListOf<Int>()
+        repeat(3) {
+            topThreeTotalList.add(totalCaloriesList.max())
+            totalCaloriesList.remove(totalCaloriesList.max())
+        }
+        return topThreeTotalList.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
