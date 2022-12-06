ff4a3615871e3bd7ed2da4b2654cda1eb958329d
Harry Angus Cliff Jr <harryacliff@gmail.com>
2022-12-06 17:36:21 -0500

    Day 4 AoC

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -4,49 +4,50 @@ import readInput
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.sumOf { bagString ->
-            val comp1 = bagString.substring(0, bagString.length / 2)
-            val comp2 = bagString.substring(bagString.length / 2, bagString.length)
+    fun parseElfSection(it: String): List<Int> {
+        return it.split("-").let {
+            (it[0].toInt() until it[1].toInt() + 1).toList()
+        }
+    }
 
-            val commonLetters = comp1.filter {
-                comp2.contains(it)
-            }.toSet().toList()
-            commonLetters.sumOf {
-                it.convertToScore()
-            }
+    fun part1(input: List<String>): Int {
+        return input.map {
+            it.split(",")
+        }.sumOf {
+            val elfString0 = it[0]
+            val elfString1 = it[1]
+            val elfSections0 = parseElfSection(elfString0)
+            val elfSections1 = parseElfSection(elfString1)
+            if (elfSections0.containsAll(elfSections1) || elfSections1.containsAll(elfSections0)) {
+                1
+            } else {
+                0
+            }.toInt()
         }
     }
 
     fun part2(input: List<String>): Int {
-        return List(input.size) { index ->
-            if (index.mod(3) == 0) {
-                val bag1 = input[index]
-                val bag2 = input[index + 1]
-                val bag3 = input[index + 2]
-                bag1.first {
-                    bag2.contains(it) && bag3.contains(it)
-                }.convertToScore()
+        return input.map {
+            it.split(",")
+        }.sumOf {
+            val elfString0 = it[0]
+            val elfString1 = it[1]
+            val elfSections0 = parseElfSection(elfString0)
+            val elfSections1 = parseElfSection(elfString1)
+            if (elfSections0.intersect(elfSections1.toSet()).isNotEmpty()) {
+                1
             } else {
                 0
-            }
-        }.sum()
+            }.toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day03", "Day03_test")
-    check(part1(testInput) == 157)
-//    check(part2(testInput) == 12)
+    val testInput = readInput("day04", "Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
-    val input = readInput("day03", "Day03")
+    val input = readInput("day04", "Day04")
     println(part1(input))
     println(part2(input))
-}
-
-private fun Char.convertToScore(): Int {
-    return this.lowercase()[0] - 'a' + 1 + if (this.isUpperCase()) {
-        26
-    } else {
-        0
-    }
 }
